ef23f860dc671b39995b4609a644acf708b516c5
Vinothini Manoharan <262053633+vinothini-07@users.noreply.github.com>
2026-02-28 13:30:39 +0530

    Add files via upload

---
+++ structures/basic structure.c
@@ -0,0 +1,17 @@
+include <stdio.h>
+
+void update(int *a,int *b) {
+     *a=*a+*b;
+     *b=abs(*a-(2**b));
+}
+
+int main() {
+    int a,b;
+    int *pa=&a, *pb=&b;
+    
+    scanf("%d\n %d\n",&a,&b);
+    update(pa,pb);
+    printf("%d\n %d\n",a,b);
+
+    return 0;
+}
